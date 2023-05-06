32895b70019af581ddce05ca173af2f531a44d66
mr_efremov_26 <51749638+mrefremov26@users.noreply.github.com>
2023-05-06 11:48:07 +0400

    add new solution

---
+++ problems/find-the-index-of-the-first-occurrence-in-a-string/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Time complexity -  O(1)
+ * Space complexity - O(1)  
+ * https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+ */
+function strStr(haystack: string, needle: string): number {
+    return haystack.indexOf(needle)
+};
